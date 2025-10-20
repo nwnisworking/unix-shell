f5c3b3c33960a9e59856a019881610ca835b68f9
nwnisworking <nwnisworking@gmail.com>
2025-10-20 15:55:15 +0800

    Set all separators in tokens and add a clearTokens function  which clears dynamically allocated separators.

--- tokens.c
+++ tokens.c
@@ -7,11 +7,25 @@ int tokenise(char* input, char* tokens[]){
 
   while(*ptr != '\0'){
     switch(*ptr){
-      case '\t' : 
-      case '\n' :
-      case ' ' : 
+      case TAB_SEP : 
+      case NEWLINE_SEP :
+      case SPACE_SEP : 
         *ptr = '\0';
         is_token = 0;
+      break;
+      case PIPE_SEP :
+      case CONCURRENT_SEP :
+      case SEQUENCE_SEP :
+        char sep = *ptr;
+
+        if(is_token){
+          *ptr = '\0';
+          is_token = 0;
+        }
+
+        tokens[count++] = strdup((char[]){sep, '\0'});
+        
+        if(count >= MAX_TOKENS) return -1;
       break;
       default : 
         if(!is_token){
@@ -26,4 +40,26 @@ int tokenise(char* input, char* tokens[]){
   }
 
   return count;
+}
+
+void clearTokens(char* tokens[]){
+  for(int i = 0; i < MAX_TOKENS; i++){
+    char* token = tokens[i];
+
+    if(token == NULL) break;
+
+    if(strlen(token) == 1 && (
+        token[0] == PIPE_SEP || 
+        token[0] == CONCURRENT_SEP ||
+        token[0] == SEQUENCE_SEP
+      )
+    ){
+      // Free strdup'ed separator tokens
+      free(token);
+      tokens[i] = NULL;
+    }
+    else{
+      tokens[i] = NULL;
+    }
+  }
 }
--- tokens.h
+++ tokens.h
@@ -3,8 +3,18 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX_TOKENS 100
+
+#define PIPE_SEP '|'
+#define CONCURRENT_SEP '&'
+#define SEQUENCE_SEP ';'
+#define TAB_SEP '\t'
+#define SPACE_SEP ' '
+#define NEWLINE_SEP '\n'
+#define REDIRECT_IN '<'
+#define REDIRECT_OUT '>'
 
 /**
  * Tokenise the input string into an array of tokens.
@@ -14,5 +24,7 @@
  * @return The number of tokens extracted. -1 if the number of tokens exceeds MAX_TOKENS.
  */
 int tokenise(char* input, char* tokens[]);
+
+void clearTokens(char* tokens[]);
 
 #endif // TOKENS_H
