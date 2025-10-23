06bbf8d0f190d27ad8a259332fea918bcd0d9607
nwnisworking <nwnisworking@gmail.com>
2025-10-23 10:01:03 +0800

    Added the ability to change prompt. Also moved exit.

--- main.c
+++ main.c
@@ -11,22 +11,23 @@ int main(){
   Command commands[MAX_NUM_COMMANDS] = {0};
   size_t len;
 
+  char prompt[MAX_LINE_LENGTH] = "%";
+
   while(1){
     // Clean up tokens and commands from previous iteration
     clearTokens(tokens);
     clearCommands(commands);
 
-    printf("$ ");
+    printf("%s ", prompt);
 
     if(!fgets(line, MAX_LINE_LENGTH, stdin)) break;
 
     len = getInput(line);
 
-    if(strcmp(line, "exit") == 0) break;
-
     int num_tokens = tokenise(line, tokens);
     int num_commands = separateCommands(tokens, commands);
 
+    // Debug
     printf("Number of commands: %d\n", num_commands);
     for(int i = 0; i < num_commands; i++){
       printf(
@@ -41,6 +42,27 @@ int main(){
 
       for(int j = 0; commands[i].argv != NULL && commands[i].argv[j] != NULL; j++){
         printf("  argv[%d]: %s\n", j, commands[i].argv[j]);
+      }
+    }
+
+    // End of debug
+
+    // For now, if the command parsing fails, just continue.
+    if(num_commands <= 0) continue;
+
+    for(int i = 0; i < num_commands; i++){
+      Command cmd = commands[i];
+
+      if(strcmp(cmd.argv[0], "exit") == 0){
+        return 0;
+      }
+      else if(strcmp(cmd.argv[0], "prompt") == 0){
+        if(cmd.argv[1] != NULL){
+          strncpy(prompt, cmd.argv[1], strlen(cmd.argv[1]) + 1);
+        }
+        else{
+          strncpy(prompt, "%", 2);
+        }
       }
     }
   }
